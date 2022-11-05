825862871e3a5e33ea3737c6332c7ca55aa38729
qirele <kirilczikzik@gmail.com>
2022-11-05 20:16:30 +0100

    add backspace

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const btns = document.querySelectorAll(".buttons button");
 const display = document.querySelector(".display-inner");
 
-btns.forEach(btn => btn.addEventListener("click", takeAction));
+btns.forEach((btn) => btn.addEventListener("click", takeAction));
 
 let displayValue = "";
 let operator = "";
@@ -10,36 +10,38 @@ let shouldClearNext = false;
 function takeAction(e) {
   let action = e.target.textContent;
 
+  if (action === "<") {
+    backspace();
+    return;
+  }
+
+  if (display.textContent.includes("ERR") || action === "CE") {
+    clear();
+    return;
+  }
+
   if (action === ".") {
     // check if a dot already appears in value
     if (!display.textContent.includes(".")) {
       display.textContent += ".";
-    } 
+    }
 
     return;
   }
 
-
   if (action === "=") {
     if (displayValue !== "" && operator !== "") {
-      displayValue = operate(operator, parseFloat(displayValue), parseFloat(display.textContent));
-      display.textContent  = displayValue;
+      displayValue = operate( operator, parseFloat(displayValue), parseFloat(display.textContent) );
+      display.textContent = displayValue;
       operator = "";
     }
     return;
   }
-  if (action === "CE") {
-    displayValue = "";
-    operator = "";
-    display.textContent = "0";
-    return;
-  }
 
   if (e.target.className.includes("operator")) {
-
     shouldClearNext = true;
     if (displayValue !== "" && operator !== "") {
-      displayValue = operate(operator, parseFloat(displayValue), parseFloat(display.textContent))
+      displayValue = operate( operator, parseFloat(displayValue), parseFloat(display.textContent) );
       display.textContent = displayValue;
     } else {
       displayValue = display.textContent;
@@ -47,7 +49,6 @@ function takeAction(e) {
     operator = action;
     return;
   }
-
 
   /**** WE CLICKED ON A NUMBER */
   /**if last action was operator, we should clear displayText now */
@@ -60,41 +61,63 @@ function takeAction(e) {
   display.textContent += e.target.textContent;
 }
 
+function clear() {
+  displayValue = "";
+  operator = "";
+  display.textContent = "0";
+}
+
+function backspace() {
+  if (display.textContent === "0") {
+    return;
+  }
+
+  let output = display.textContent.slice(0, -1);
+  if (output === "") {
+    display.textContent = "0";
+    return;
+  }
+
+  display.textContent = output;
+
+
+
+}
 
 
 /*========== CORE CALCULATOR FUNCTIONS ============*/
-function add(x,y) {
+function add(x, y) {
   return x + y;
 }
 
-function subtract(x,y) {
+function subtract(x, y) {
   return x - y;
 }
 
-function multiply(x,y) {
+function multiply(x, y) {
   return x * y;
 }
 
-function divide(x,y) {
+function divide(x, y) {
+  if (y === 0) return "ERR: divide by zero";
   return x / y;
 }
 
-
-function operate(operator, x,y) {
-  switch(operator) {
+function operate(operator, x, y) {
+  switch (operator) {
     case "+":
-      return add(x,y);
+      return add(x, y);
       break;
     case "-":
-      return subtract(x,y);
+      return subtract(x, y);
       break;
     case "*":
-      return multiply(x,y);
+      return multiply(x, y);
       break;
     case "/":
-      return divide(x,y);
+      return divide(x, y);
       break;
-    default: 
+    default:
       return "ERROR";
   }
 }
